5859c91024b6053a83d6085ab9c8e2d108423f45
Kayode Oluwafemi <mrlapinho@gmail.com>
2020-06-23 06:51:32 +0100

    Implement Mobile responsiveness

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const nav = document.querySelector(".nav");
+const hamburgerMenu = document.querySelector(".nav__menu");
+const navGroup = document.querySelector(".nav__group");
+
+// toggle nav on mobile devices
+hamburgerMenu.addEventListener("click", (e) => {
+  e.preventDefault();
+  navGroup.classList.toggle("open");
+});
 
 // Change nav background color on window scroll
 window.addEventListener("scroll", () => {
