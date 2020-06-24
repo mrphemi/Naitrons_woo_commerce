de00576a970f0bcfaf7e55406499bc63a19d2404
Kayode Oluwafemi <mrlapinho@gmail.com>
2020-06-24 10:18:42 +0100

    Add close button for mobile nav navigation

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const nav = document.querySelector(".nav");
-const hamburgerMenu = document.querySelector(".nav__menu");
+const hamburgerMenu = document.querySelector(".nav__menu .open-nav");
 const navGroup = document.querySelector(".nav__group");
+const closeIcon = document.querySelector(".nav__menu .close-nav");
 
-// toggle nav on mobile devices
-hamburgerMenu.addEventListener("click", (e) => {
-  e.preventDefault();
-  navGroup.classList.toggle("open");
+// open nav on mobile devices
+hamburgerMenu.addEventListener("click", () => {
+  navGroup.classList.add("open");
+  hamburgerMenu.classList.add("hide");
+  closeIcon.classList.remove("hide");
+});
+
+// open nav on mobile devices
+closeIcon.addEventListener("click", () => {
+  navGroup.classList.remove("open");
+  closeIcon.classList.add("hide");
+  hamburgerMenu.classList.remove("hide");
 });
 
 // Change nav background color on window scroll
